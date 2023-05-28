624aade39ab7705909d8b369740aad2e9a3e520a
xenedium <ahmed@abderraziq.com>
2023-05-28 20:35:43 +0100

    Message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,19 @@
 import { HeaderMenu } from '@/components/Header';
-import { AppShell } from '@mantine/core';
+import { AppShell, Container, Title } from '@mantine/core';
 import { useAuth } from '@/hooks';
 
 export default function Home() {
     const { user } = useAuth();
 
     return (
-
         <AppShell
             padding="md"
             header={<HeaderMenu {...user} />}
         >
+            <Container>
+                <Title align='center'>No subreddits found...</Title>
+                <Title align='center'>Create one</Title>
+            </Container>
         </AppShell>
 
     );
