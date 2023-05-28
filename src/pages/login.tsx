4bcee71e95ef2111eee24f1a8d6b335fac293cec
xenedium <ahmed@abderraziq.com>
2023-05-28 20:29:43 +0100

    Added waves

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,8 +6,7 @@ const useStyles = createStyles((theme) => ({
     wrapper: {
         minHeight: rem(900),
         backgroundSize: 'cover',
-        backgroundImage:
-            'url(https://images.unsplash.com/photo-1484242857719-4b9144542727?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1280&q=80)',
+        backgroundImage: 'url(/waves.svg)',
     },
 
     form: {
